96782a1b68519846afe7b1afd9355442d35ecbfd
Oleksii Aleksandrov <pupkin21081994@gmail.com>
2018-05-01 12:03:30 +0200

    create BrowserForm

---
+++ NewsAggregator/BrowserForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NewsAggregator
+{
+    public partial class BrowserForm : Form
+    {
+        public BrowserForm(Uri uri)
+        {
+            InitializeComponent();
+            this.webBrowser.Navigate(uri);
+        }
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            Console.WriteLine(e.Url);
+            callback();
+        }
+    }
+}
