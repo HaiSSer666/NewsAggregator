66805a2a8fcbb8d680b43cdc05fb09a991e6b7ab
Oleksii Aleksandrov <pupkin21081994@gmail.com>
2018-05-05 14:27:45 +0200

    add Error as parameter to PublishCallback

--- NewsAggregator/Classes/SharedTypes.cs
+++ NewsAggregator/Classes/SharedTypes.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NewsAggregator
+﻿namespace NewsAggregator
 {
     public delegate void LoginCallback(Error error);
-    public delegate void PublishCallback();
+    public delegate void PublishCallback(Error error);
     public delegate void RestoreCallback();
 }
