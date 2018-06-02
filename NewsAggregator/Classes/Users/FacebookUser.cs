8b9b38c710fbf2f100aa4980874fe7a49ee0ec2f
Oleksii Aleksandrov <pupkin21081994@gmail.com>
2018-06-02 11:41:44 +0200

    add user classes

---
+++ NewsAggregator/Classes/Users/FacebookUser.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace NewsAggregator
+{
+    internal class FacebookUser
+    {
+        [JsonProperty(PropertyName = "first_name")]
+        public string firstName { get; set; }
+        public Picture picture { get; set; }
+        public string email { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Data
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Picture
+    {
+        public Data data { get; set; }
+    }
+}
---
+++ NewsAggregator/Classes/Users/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsAggregator
+{
+    class User
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+    }
+}
