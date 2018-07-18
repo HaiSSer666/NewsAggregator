f9a9320c7aa2aa27b98dacec715fa404fcb91e73
Oleksii Aleksandrov <pupkin21081994@gmail.com>
2018-07-18 21:29:30 +0200

    implement IFacebookFeedManager

--- NewsAggregator/Classes/Facades/FeedFacade.cs
+++ NewsAggregator/Classes/Facades/FeedFacade.cs
@@ -3,25 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NewsAggregator.Classes.Managers;
 
 namespace NewsAggregator
 {
     public class FeedFacade
     {
         TweeterFeedManager tweeterFeedManager = new TweeterFeedManager();
+        FacebookFeedManager facebookFeedManager = new FacebookFeedManager();
+        IFacebookFeedManager fbFeedManager;
 
-        public void GetFeed(SocialNetwork socialNetwork, int maximumTweets, FeedCallback feedCallback)
+        public FeedFacade(IFacebookFeedManager fbFeedManager)
+        {
+            this.fbFeedManager = fbFeedManager;
+        }
+
+        public void GetFeed(SocialNetwork socialNetwork, FeedCallback feedCallback)
         {
             switch (socialNetwork)
             {
                 case SocialNetwork.Tweeter:
                     {
-                        tweeterFeedManager.GetFeed(maximumTweets, feedCallback);
+                        tweeterFeedManager.GetFeed(feedCallback);
                         break;
                     }
                 case SocialNetwork.Facebook:
                     {
-                        return;
+                        //facebookFeedManager.GetFeed(feedCallback);
+                        fbFeedManager.GetFeed(feedCallback);
+                        break;
                     }
                 default:
                     {
